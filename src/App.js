248ee1f238bed7e0e9135d030928080f0a2dd72b
MUHAMMAD WASEEM ARIF <wasimarif602@gmail.com>
2021-06-08 10:44:28 +0500

    added react router components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 
 function App() {
+  const Home = () => (<div>This is Home</div>)
+  const Blog = () => (<div>This is Blog</div>)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul className="navbar">
+          <NavLink as="li" to="/">Home</NavLink>
+          <NavLink as="li" to="blog">Blog</NavLink>
+        </ul>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="blog" element={<Blog />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
